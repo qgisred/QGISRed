73294b5c76176e2d3d0e96912abfee1f15ecff3e
neslerel <aguilerma@gmail.com>
2022-09-16 15:22:09 +0200

    New no official version (0.16.2)

--- qgisred_plugins.py
+++ qgisred_plugins.py
@@ -75,7 +75,7 @@ class QGISRed:
     especificComplementaryLayers = []
     complementaryLayers = ["IsolationValves", "Hydrants", "WashoutValves", "AirReleaseValves", "ServiceConnections", "Meters"]
     TemporalFolder = "Temporal folder"
-    DependenciesVersion = "1.0.16.1"
+    DependenciesVersion = "1.0.16.2"
     gisredDll = None
 
     """Basic"""
