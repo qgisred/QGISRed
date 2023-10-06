129c2bcdd63fc4662a706c4bddd8f988790b5ce0
Fernando Martinez Alzamora <fmartine@hma.upv.es>
2023-10-06 14:25:32 +0200

    Release 0.16.10 beta version

--- qgisred_plugins.py
+++ qgisred_plugins.py
@@ -75,7 +75,7 @@ class QGISRed:
     especificComplementaryLayers = []
     complementaryLayers = ["IsolationValves", "Hydrants", "WashoutValves", "AirReleaseValves", "ServiceConnections", "Meters"]
     TemporalFolder = "Temporal folder"
-    DependenciesVersion = "1.0.16.9"
+    DependenciesVersion = "1.0.16.10"
     gisredDll = None
 
     """Basic"""
