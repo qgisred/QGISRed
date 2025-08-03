bcaaa090f262b38d8a9637553f862aa0337e87e5
Fernando Martinez Alzamora <fmartine@hma.upv.es>
2025-08-03 10:30:09 +0200

    Changes for compatibility up to version 3.44 of QGIS

--- tools/qgisred_editLinksGeometry.py
+++ tools/qgisred_editLinksGeometry.py
@@ -6,7 +6,10 @@ from qgis.gui import QgsMapTool, QgsVertexMarker, QgsRubberBand, QgsMapCanvasSna
 try:
     from qgis.gui import Qgis
 except:
-    pass
+    try:
+        from qgis.core import Qgis # Compatibility with QGis 3.4x
+    except:
+        pass
 
 from ..tools.qgisred_utils import QGISRedUtils
 
