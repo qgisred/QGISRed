e44eeab6a6aa5445e7558483c5eeff7544c27ffc
Vitor Vieira <vitorestradas@hotmail.com>
2026-02-13 08:06:26 +0100

    Window Update and new dialog format for thematic maps

--- ui/qgisred_thematicmaps_dialog.py
+++ ui/qgisred_thematicmaps_dialog.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 from PyQt5.QtWidgets import QDialog
+from PyQt5.QtGui import QIcon
 from qgis.PyQt import uic
 import os
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(os.path.dirname(__file__), "qgisred_thematicmaps_dialog.ui"))
-
 
 class QGISRedThematicMapsDialog(QDialog, FORM_CLASS):
     def __init__(self, parent=None):
         """Constructor."""
         super(QGISRedThematicMapsDialog, self).__init__(parent)
         self.setupUi(self)
+
+        # Set window icon
+        icon_path = os.path.join(os.path.dirname(__file__), '..', 'images', 'iconThematicMaps.png')
+        self.setWindowIcon(QIcon(icon_path))
